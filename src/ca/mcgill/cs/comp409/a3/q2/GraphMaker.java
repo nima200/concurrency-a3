0229561fc8b5b63e1f96e7c2e83ea25cc696650c
Nima Adibpour <nima.adibpour@mail.mcgill.ca>
2018-03-28 10:21:25 -0400

    Fix Q2 errors

--- src/ca/mcgill/cs/comp409/a3/q2/GraphMaker.java
+++ src/ca/mcgill/cs/comp409/a3/q2/GraphMaker.java
@@ -1,29 +1,22 @@
 package ca.mcgill.cs.comp409.a3.q2;
 
 import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class GraphMaker {
     public static List<Node> ConstructGraph(int n, int e) {
         List<Node> nodes = new ArrayList<>();
-        HashMap<Integer, HashSet<Integer>> adj_map = new HashMap<>();
-        Random rand = new Random();
         for (int i = 0; i < n; i++) {
             nodes.add(new Node(i));
         }
         for (int i = 0; i < e; i++) {
-            int vertex1 = rand.nextInt(n);
-            int vertex2 = (vertex1 + rand.nextInt(n - 1) + 1) % nodes.size();
-            // Lazy load elements into adjacency map
-            if (!adj_map.containsKey(vertex1)) adj_map.put(vertex1, new HashSet<>());
-            if (!adj_map.containsKey(vertex2)) adj_map.put(vertex2, new HashSet<>());
+            int vertex1 = ThreadLocalRandom.current().nextInt(n);
+            int vertex2 = vertex1 + ThreadLocalRandom.current().nextInt(1, n - 1) % nodes.size();
 
-            if (adj_map.get(vertex1).contains(vertex2)) {
+            if (nodes.get(vertex1).getNeighbors().contains(nodes.get(vertex2))) {
                 i--;
                 continue;
             }
-
-            adj_map.get(vertex1).add(vertex2);
-            adj_map.get(vertex2).add(vertex1);
 
             nodes.get(vertex1).addNeighbor(nodes.get(vertex2));
         }
